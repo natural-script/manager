70a29db16cd8a5c534cb9bd1d9bf077fb541c537
GeeekyBoy <ahmed__hassan@outlook.com>
2017-08-29 10:44:26 +0200

    Developing The Manager UI

--- src/index.js
+++ src/index.js
@@ -6,101 +6,166 @@ const path = require('path');
 const express = require('express');
 const staticGzip = require('http-static-gzip-regexp');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const AppDirectory = require('appdirectory')
 const cors_proxy = require('cors-anywhere');
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(bodyParser());
+const Store = require('server-store');
+const dirs = new AppDirectory({
+  appName: "JsteManager",
+  appAuthor: "JsteTeam",
+  appVersion: "v1.0.0"
+});
+const appConfigDir = dirs.userConfig();
+const managerConfigDB = new Store('JsteManager', 'Config', appConfigDir);
 const currentPlatform = os.platform();
 const currentArch = os.arch();
 const root = path.join(__dirname, 'assets');
 const binPath = process.execPath;
-const optionDefinitions = [
-  { name: 'install', alias: 'i', type: Boolean }
-];
+const optionDefinitions = [{
+  name: 'install',
+  alias: 'i',
+  type: Boolean
+}];
 const options = commandLineArgs(optionDefinitions);
 
 if (options.install) {
-console.log("");
-console.log("");
-console.log("");
-console.log(" Installing Jste Manager for " + currentPlatform + " " + currentArch);
-console.log("");
-console.log("");
-console.log("");
-console.log(" Copying Jste Files ");
-if (currentPlatform == 'linux') {
-shell.cp('-R', 'JsteManager', '/etc/init.d/');
-console.log("");
-console.log("");
-console.log("");
-console.log(" Registering Jste Manager script to init.d ");
-shell.chmod('ugo+x', '/etc/init.d/JsteManager');
-shell.exec('update-rc.d JsteManager defaults');
-console.log("");
-console.log("");
-console.log("");
-console.log(" Launching Jste Manager ");
-shell.exec('/etc/init.d/JsteManager &');
-console.log("");
-console.log("");
-console.log("");
-console.log("");
-console.log("================================================");
-console.log(" Jste Manager has been installed successfuly ;) ");
-console.log("================================================");
-console.log("");
+  console.log("");
+  console.log("");
+  console.log("");
+  console.log(" Installing Jste Manager for " + currentPlatform + " " + currentArch);
+  console.log("");
+  console.log("");
+  console.log("");
+  console.log(" Copying Jste Files ");
+  if (currentPlatform == 'linux') {
+    shell.cp('-R', 'JsteManager', '/etc/init.d/');
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log(" Registering Jste Manager script to init.d ");
+    shell.chmod('ugo+x', '/etc/init.d/JsteManager');
+    shell.exec('update-rc.d JsteManager defaults');
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log(" Launching Jste Manager ");
+    shell.exec('/etc/init.d/JsteManager &');
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("================================================");
+    console.log(" Jste Manager has been installed successfuly ;) ");
+    console.log("================================================");
+    console.log("");
 
-} else if (currentPlatform == 'freebsd') {
-shell.cp('-R', 'JsteManager', '/etc/init.d/');
-console.log("");
-console.log("");
-console.log("");
-console.log(" Registering Jste Manager script to init.d ");
-shell.chmod('ugo+x', '/etc/init.d/JsteManager');
-shell.exec('update-rc.d JsteManager defaults');
-console.log("");
-console.log("");
-console.log("");
-console.log(" Launching Jste Manager ");
-shell.exec('/etc/init.d/JsteManager &');
-console.log("");
-console.log("");
-console.log("");
-console.log("");
-console.log("================================================");
-console.log(" Jste Manager has been installed successfuly ;) ");
-console.log("================================================");
-console.log("");
-} else if (currentPlatform == 'win32') {
-shell.cp('-R', 'JsteManager.exe', '/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp');
-console.log("");
-console.log("");
-console.log("");
-console.log(" Launching Jste Manager ");
-shell.exec('start "" "/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/JsteManager.exe"');
-console.log("");
-console.log("");
-console.log("");
-console.log("");
-console.log("================================================");
-console.log(" Jste Manager has been installed successfuly ;) ");
-console.log("================================================");
-console.log("");
-}
-}else {
+  } else if (currentPlatform == 'freebsd') {
+    shell.cp('-R', 'JsteManager', '/etc/init.d/');
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log(" Registering Jste Manager script to init.d ");
+    shell.chmod('ugo+x', '/etc/init.d/JsteManager');
+    shell.exec('update-rc.d JsteManager defaults');
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log(" Launching Jste Manager ");
+    shell.exec('/etc/init.d/JsteManager &');
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("================================================");
+    console.log(" Jste Manager has been installed successfuly ;) ");
+    console.log("================================================");
+    console.log("");
+  } else if (currentPlatform == 'win32') {
+    shell.cp('-R', 'JsteManager.exe', '/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp');
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log(" Launching Jste Manager ");
+    shell.exec('start "" "/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/JsteManager.exe"');
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("================================================");
+    console.log(" Jste Manager has been installed successfuly ;) ");
+    console.log("================================================");
+    console.log("");
+  }
+} else {
 
-app.use(staticGzip(/(framework\.min\.html|\.js|\.css)$/));  
+  app.use(staticGzip(/(framework\.min\.html)$/));
 
-app.use(express.static(root));
+  app.use(express.static(root));
 
-var server = app.listen(5050, function () {
-});
+  var server = app.listen(5050, function () {});
 
-var localAddress = '0.0.0.0' || 'localhost'; 
-cors_proxy.createServer({
+  app.post('/setAdminPassword', function (req, res) {
+    if (managerConfigDB.getItem('adminPassword')) {
+      if (managerConfigDB.getItem('adminPassword') == req.body.oldPassword) {
+        managerConfigDB.setItem('adminPassword', req.body.newPassword);
+        res.send('The admin password has been set successfuly ;)');
+      } else {
+        res.send('Authentication failed :(');
+      }
+    } else {
+      managerConfigDB.setItem('adminPassword', req.body.newPassword);
+      res.send('The admin password has been set successfuly ;)');
+    }
+  });
+
+  app.post('/childModeActivate', function (req, res) {
+    if (req.body.adminPassword && req.body.adminPassword == managerConfigDB.getItem('adminPassword')) {
+      managerConfigDB.setItem('childMode', 'on');
+      res.send('Child mode has been activated successfuly ;)');
+    } else {
+      res.send('Authentication failed :(');
+    }
+  });
+
+  app.post('/childModeDeactivate', function (req, res) {
+    if (req.body.adminPassword && req.body.adminPassword == managerConfigDB.getItem('adminPassword')) {
+      managerConfigDB.setItem('childMode', 'off');
+      res.send('Child mode has been deactivated successfuly ;)');
+    } else {
+      res.send('Authentication failed :(');
+    }
+  });
+
+  app.get('/childModeStatus', function (req, res) {
+    res.send(managerConfigDB.getItem('childMode'));
+  });
+
+  app.get('/adminPasswordStatus', function (req, res) {
+    if (managerConfigDB.getItem('adminPassword')) {
+      res.send('set');
+    } else {
+      res.send('not set');
+    }
+  });
+
+  app.post('/adminPasswordVerification', function (req, res) {
+    if (managerConfigDB.getItem('adminPassword') == req.body.adminPassword) {
+      res.send('You have been logged in successfuly ;)');
+    } else {
+      res.send('Authentication failed :(');
+    }
+  });
+
+  var localAddress = '0.0.0.0' || 'localhost';
+  cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: [],
-    setHeaders: {"Access-Control-Expose-Headers": "Content-Length"},
+    setHeaders: {
+      "Access-Control-Expose-Headers": "Content-Length"
+    },
     removeHeaders: ['cookie', 'cookie2']
-}).listen(6060, localAddress, function() {
-});
+  }).listen(6060, localAddress, function () {});
 }
