d34622e4df425ea5ec3d7c635953572a1d9b1420
GeeekyBoy <ahmed__hassan@outlook.com>
2018-11-15 00:48:54 +0200

    Updating Framework Files

--- build.js
+++ build.js
@@ -7,6 +7,6 @@ console.log(data);
 console.log(' Starting building Jste Manager ');
 shell.rm('-rf', 'build/*');
 shell.cd('src');
-shell.exec('pkg index.js --out-path ../build -c ../package.json');
+shell.exec('npx pkg index.js --out-path ../build -c ../package.json');
 console.log(' Jste Manager has been built properly ;) ');
 });
